4dca2cd41bd9bcd9a6fca3617a4878c8f1dd3f22
Antoine <ant.brrrr@gmail.com>
2025-11-12 15:31:55 +0100

    script input birth

--- post_auth/script/change_input_auto.js
+++ post_auth/script/change_input_auto.js
@@ -11,15 +11,44 @@ const bouton = document.getElementById("submit-btn");
 
 input1_field.addEventListener("input", function() {
     const input1 = input1_field.value;
+    const restrictions_ipt1 = input1 === "0" || input1 === "1" || input1 === "2" || input1 === "3";
 
-    if (input1 !== "") {
+    if (
+        input1 !== "" &&
+        restrictions_ipt1
+    ) {
         input2_field.focus();
         input1_field.classList.add("filled");
+    } else {
+        if (!restrictions_ipt1) {
+            input1_field.value = "";
+        }
+    }
+
+    if (input1 == "") {
+        input1_field.classList.remove("filled");
     }
 })
 
 input2_field.addEventListener("input", function() {
     const input2 = input2_field.value;
+    const restrictions_ipt2 = /\d/.test(input2);
 
-    if ()
+    if (
+        input1_field.classList.contains("filled") && 
+        input2 !== "" &&
+        restrictions_ipt2
+    ) {
+        input3_field.focus();
+        input2_field.classList.add("filled");
+    } else {
+        if (!restrictions_ipt2) {
+            input2_field.value = "";
+        }
+    }
+
+    if (input2 == "") {
+        input2_field.classList.remove("filled");
+    } 
 })
+
